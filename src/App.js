1713e3283795b6eef171731d1ad718684491e072
Stephen Young <stephenjayyoung@gmail.com>
2020-06-26 16:01:16 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Note from './components/Note'
 import './App.css';
 
 class App extends Component {
@@ -7,32 +8,71 @@ class App extends Component {
     super(props);
     this.state = {
       noteText: '',
+      notes: [],
+      amountLeft: 0,
     } 
   }
   updateNoteText(noteText) {
     this.setState({noteText: noteText.target.value})
   }
 
-  handleKeyPress = (event) => {
-    if (event.key ==="Enter"){
-
-
-    }
+  addNote() {
+    let notesArr = this.state.notes;
+    notesArr.push(this.state.noteText);
+    this.setState({ noteText: "" });
+    this.textInput.focus();
+    if(this.state.noteText === "") {return false}
   }
 
+  addCounter() {
+    let counter = this.state.amountLeft;
+    counter +=1;
+    // this.setState({counter ++ 1})
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key ==="Enter"){
+      console.log('enter pressed');
+      let notesArr = this.state.notes;
+      notesArr.push(this.state.noteText);
+      this.setState({ noteText: "" });
+      this.textInput.focus();
+    }
+
+  }
+
+  deleteNote(index) {
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1);
+    this.setState({notes : notesArr })
+  } 
+
 render() {
+
+let notes = this.state.notes.map((val, key) =>{
+  return <Note key={key} text={val}
+    deleteMethod={ () => this.deleteNote(key) } />
+
+
+})
+
   return (
     <div className="app">
       <div className = "header">
-        React TODO Application
+        <h1>React TODO Application</h1>
       </div>
-      <div className = "btn">+</div>
+      <div className = "remaining">
+        <h3 onClick={this.addCounter.bind(this)}>{this.state.amountLeft} tasks remaining</h3>
+      </div>      
+      {notes}
+
+      <div className = "btn" onClick={this.addNote.bind(this)}>+</div>
 
       <input type="text"
         ref={((input) => {this.textInput = input})} 
         className="textInput"
         value={this.state.noteText}
-        onChangeText={noteText => this.updateNoteText(noteText)}
+        onChange={noteText => this.updateNoteText(noteText)}
         onKeyPress={this.handleKeyPress.bind(this)}
         />
 
